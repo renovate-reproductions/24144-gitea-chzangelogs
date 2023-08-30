8aee8151fcfd01f51a595d45832eb16bc546c62f
Michael Kriese <michael.kriese@visualon.de>
2023-08-30 10:47:51 +0200

    chore: fix repo url

--- test.tf
+++ test.tf
@@ -7,7 +7,7 @@ resource "helm_release" "gitea" {
 
 resource "helm_release" "forgejo" {
   name       = "forgejo"
-  repository = "oci://codeberg.org/forgejo-contrib/forgejo"
+  repository = "oci://codeberg.org/forgejo-contrib"
   chart      = "forgejo"
   version    = "0.8.4"
 }
